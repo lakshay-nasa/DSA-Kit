66d2d4c0d9ee5482e32f7872fe9477a5a18d596c
lakshaynasa <lakshay1717@gmail.com>
2022-09-06 22:32:54 +0530

    BST Insertion

---
+++ DSA Expedition/Trees/Binary Search Tree/Insertion.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void Insert(struct node* root, int key){
+    struct node *prev = NULL;
+    struct node *ptr;
+    while (root!=NULL)
+    {
+        prev = root;
+        if (key == root->data){
+            printf("Key already exist. Insertion not possible.");
+        }
+
+        else if(key<root->data){
+            root = root->left;
+        }
+
+        else{
+            root = root->right;
+        }
+    }
+
+    struct node* newData = createNode(key);
+    if(key > prev->data){
+        prev->right = newData;
+    }
+    else{
+        prev->left = newData;
+    }
+
+}
+
+int main()
+{
+
+    struct node *p = createNode(9);
+    struct node *p1 = createNode(5);
+    struct node *p2 = createNode(12);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(18);
+
+    // Tree Looks Like -->
+    //        9
+    //       / \
+    //      5   12
+    //     / \    \
+    //    1   7   18
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    Insert(p, 10);
+    printf("%d", p->right->left->data);
+    return 0;
+}
